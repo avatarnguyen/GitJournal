f26e082a2aa8b9fa9576117cffdf9002fec6788e
anhNEON <anh@neon.dev>
2022-05-17 12:52:05 +0200

    delete observer list

--- lib/core/folder/notes_folder_notifier.dart
+++ lib/core/folder/notes_folder_notifier.dart
@@ -19,123 +19,124 @@ typedef NoteRenamedCallback = void Function(
     int index, Note note, String oldPath);
 
 class NotesFolderNotifier implements ChangeNotifier {
-  ObserverList<void Function(int, NotesFolder)>? _folderAddedListeners =
-      ObserverList<FolderNotificationCallback>();
-  ObserverList<void Function(int, NotesFolder)>? _folderRemovedListeners =
-      ObserverList<FolderNotificationCallback>();
-  ObserverList<void Function(NotesFolderFS, String)>?
-      _thisFolderRenamedListeners = ObserverList<FolderRenamedCallback>();
-
-  ObserverList<void Function(int, Note)>? _noteAddedListeners =
-      ObserverList<NoteNotificationCallback>();
-  ObserverList<void Function(int, Note)>? _noteRemovedListeners =
-      ObserverList<NoteNotificationCallback>();
-  ObserverList<void Function(int, Note)>? _noteModifiedListeners =
-      ObserverList<NoteNotificationCallback>();
-  ObserverList<void Function(int, Note, String)>? _noteRenameListeners =
-      ObserverList<NoteRenamedCallback>();
-
-  void addFolderRemovedListener(FolderNotificationCallback listener) {
-    if (_folderRemovedListeners != null) {
-      _folderRemovedListeners!.add(listener);
-    }
-  }
-
-  void removeFolderRemovedListener(FolderNotificationCallback listener) {
-    if (_folderRemovedListeners != null) {
-      assert(_folderRemovedListeners!.contains(listener));
-      var _ = _folderRemovedListeners!.remove(listener);
-    }
-  }
-
-  void addFolderAddedListener(FolderNotificationCallback listener) {
-    if (_folderAddedListeners != null) {
-      _folderAddedListeners!.add(listener);
-    }
-  }
-
-  void removeFolderAddedListener(FolderNotificationCallback listener) {
-    if (_folderAddedListeners != null) {
-      assert(_folderAddedListeners!.contains(listener));
-      var _ = _folderAddedListeners!.remove(listener);
-    }
-  }
-
-  void addThisFolderRenamedListener(FolderRenamedCallback listener) {
-    if (_thisFolderRenamedListeners != null) {
-      _thisFolderRenamedListeners!.add(listener);
-    }
-  }
-
-  void removeThisFolderRenamedListener(FolderRenamedCallback listener) {
-    if (_thisFolderRenamedListeners != null) {
-      assert(_thisFolderRenamedListeners!.contains(listener));
-      var _ = _thisFolderRenamedListeners!.remove(listener);
-    }
-  }
-
-  void addNoteAddedListener(NoteNotificationCallback listener) {
-    if (_noteAddedListeners != null) {
-      _noteAddedListeners!.add(listener);
-    }
-  }
-
-  void removeNoteAddedListener(NoteNotificationCallback listener) {
-    if (_noteAddedListeners != null) {
-      assert(_noteAddedListeners!.contains(listener));
-      var _ = _noteAddedListeners!.remove(listener);
-    }
-  }
-
-  void addNoteRemovedListener(NoteNotificationCallback listener) {
-    if (_noteRemovedListeners != null) {
-      _noteRemovedListeners!.add(listener);
-    }
-  }
-
-  void removeNoteRemovedListener(NoteNotificationCallback listener) {
-    if (_noteRemovedListeners != null) {
-      assert(_noteRemovedListeners!.contains(listener));
-      var _ = _noteRemovedListeners!.remove(listener);
-    }
-  }
-
-  void addNoteModifiedListener(NoteNotificationCallback listener) {
-    if (_noteModifiedListeners != null) {
-      _noteModifiedListeners!.add(listener);
-    }
-  }
-
-  void removeNoteModifiedListener(NoteNotificationCallback listener) {
-    if (_noteModifiedListeners != null) {
-      assert(_noteModifiedListeners!.contains(listener));
-      var _ = _noteModifiedListeners!.remove(listener);
-    }
-  }
-
-  void addNoteRenameListener(NoteRenamedCallback listener) {
-    if (_noteRenameListeners != null) {
-      _noteRenameListeners!.add(listener);
-    }
-  }
-
-  void removeNoteRenameListener(NoteRenamedCallback listener) {
-    if (_noteRenameListeners != null) {
-      assert(_noteRenameListeners!.contains(listener));
-      var _ = _noteRenameListeners!.remove(listener);
-    }
-  }
+  // ObserverList<void Function(int, NotesFolder)>? _folderAddedListeners =
+  //     ObserverList<FolderNotificationCallback>();
+  // ObserverList<void Function(int, NotesFolder)>? _folderRemovedListeners =
+  //     ObserverList<FolderNotificationCallback>();
+  // ObserverList<void Function(NotesFolderFS, String)>?
+  //     _thisFolderRenamedListeners = ObserverList<FolderRenamedCallback>();
+  //
+  // ObserverList<void Function(int, Note)>? _noteAddedListeners =
+  //     ObserverList<NoteNotificationCallback>();
+  // ObserverList<void Function(int, Note)>? _noteRemovedListeners =
+  //     ObserverList<NoteNotificationCallback>();
+  // ObserverList<void Function(int, Note)>? _noteModifiedListeners =
+  //     ObserverList<NoteNotificationCallback>();
+  // ObserverList<void Function(int, Note, String)>? _noteRenameListeners =
+  //     ObserverList<NoteRenamedCallback>();
+  //
+  // void addFolderRemovedListener(FolderNotificationCallback listener) {
+  //   if (_folderRemovedListeners != null) {
+  //     _folderRemovedListeners!.add(listener);
+  //   }
+  // }
+  //
+  // void removeFolderRemovedListener(FolderNotificationCallback listener) {
+  //   if (_folderRemovedListeners != null) {
+  //     assert(_folderRemovedListeners!.contains(listener));
+  //     var _ = _folderRemovedListeners!.remove(listener);
+  //   }
+  // }
+  //
+  // void addFolderAddedListener(FolderNotificationCallback listener) {
+  //   if (_folderAddedListeners != null) {
+  //     _folderAddedListeners!.add(listener);
+  //   }
+  // }
+  //
+  // void removeFolderAddedListener(FolderNotificationCallback listener) {
+  //   if (_folderAddedListeners != null) {
+  //     assert(_folderAddedListeners!.contains(listener));
+  //     var _ = _folderAddedListeners!.remove(listener);
+  //   }
+  // }
+  //
+  // void addThisFolderRenamedListener(FolderRenamedCallback listener) {
+  //   if (_thisFolderRenamedListeners != null) {
+  //     _thisFolderRenamedListeners!.add(listener);
+  //   }
+  // }
+  //
+  // void removeThisFolderRenamedListener(FolderRenamedCallback listener) {
+  //   if (_thisFolderRenamedListeners != null) {
+  //     assert(_thisFolderRenamedListeners!.contains(listener));
+  //     var _ = _thisFolderRenamedListeners!.remove(listener);
+  //   }
+  // }
+  //
+  // void addNoteAddedListener(NoteNotificationCallback listener) {
+  //   if (_noteAddedListeners != null) {
+  //     _noteAddedListeners!.add(listener);
+  //   }
+  // }
+  //
+  // void removeNoteAddedListener(NoteNotificationCallback listener) {
+  //   if (_noteAddedListeners != null) {
+  //     assert(_noteAddedListeners!.contains(listener));
+  //     var _ = _noteAddedListeners!.remove(listener);
+  //   }
+  // }
+  //
+  // void addNoteRemovedListener(NoteNotificationCallback listener) {
+  //   if (_noteRemovedListeners != null) {
+  //     _noteRemovedListeners!.add(listener);
+  //   }
+  // }
+  //
+  // void removeNoteRemovedListener(NoteNotificationCallback listener) {
+  //   if (_noteRemovedListeners != null) {
+  //     assert(_noteRemovedListeners!.contains(listener));
+  //     var _ = _noteRemovedListeners!.remove(listener);
+  //   }
+  // }
+  //
+  // void addNoteModifiedListener(NoteNotificationCallback listener) {
+  //   if (_noteModifiedListeners != null) {
+  //     _noteModifiedListeners!.add(listener);
+  //   }
+  // }
+  //
+  // void removeNoteModifiedListener(NoteNotificationCallback listener) {
+  //   if (_noteModifiedListeners != null) {
+  //     assert(_noteModifiedListeners!.contains(listener));
+  //     var _ = _noteModifiedListeners!.remove(listener);
+  //   }
+  // }
+  //
+  // void addNoteRenameListener(NoteRenamedCallback listener) {
+  //   if (_noteRenameListeners != null) {
+  //     _noteRenameListeners!.add(listener);
+  //   }
+  // }
+  //
+  // void removeNoteRenameListener(NoteRenamedCallback listener) {
+  //   if (_noteRenameListeners != null) {
+  //     assert(_noteRenameListeners!.contains(listener));
+  //     var _ = _noteRenameListeners!.remove(listener);
+  //   }
+  // }
 
   @mustCallSuper
   @override
   void dispose() {
-    _folderAddedListeners = null;
-    _folderRemovedListeners = null;
-    _thisFolderRenamedListeners = null;
-    _noteAddedListeners = null;
-    _noteRemovedListeners = null;
-    _noteModifiedListeners = null;
-    _noteRenameListeners = null;
+    // FIXME: not sure how to do this through observer classes
+    // _folderAddedListeners = null;
+    // _folderRemovedListeners = null;
+    // _thisFolderRenamedListeners = null;
+    // _noteAddedListeners = null;
+    // _noteRemovedListeners = null;
+    // _noteModifiedListeners = null;
+    // _noteRenameListeners = null;
 
     assert(_debugAssertNotDisposed());
     _listeners = null;
