87e5ec68d693ae907cb22f7930448afa82e0c6f6
anhNEON <anh@neon.dev>
2022-05-23 00:29:13 +0200

    ref: extract fillFileStorage method to separate usecase

--- lib/core/file/file_storage.dart
+++ lib/core/file/file_storage.dart
@@ -8,14 +8,10 @@ import 'dart:isolate';
 
 import 'package:dart_git/blob_ctime_builder.dart';
 import 'package:dart_git/dart_git.dart';
-import 'package:dart_git/exceptions.dart';
 import 'package:dart_git/file_mtime_builder.dart';
 import 'package:flutter/foundation.dart';
 import 'package:gitjournal/core/file/load_file_usecase.dart';
-import 'package:gitjournal/error_reporting.dart';
-import 'package:gitjournal/logger/logger.dart';
 import 'package:path/path.dart' as p;
-import 'package:tuple/tuple.dart';
 
 import 'file.dart';
 
@@ -67,8 +63,8 @@ class FileStorage with ChangeNotifier {
     });
 
     var resp = await compute(
-      _fillFileStorage,
-      _FillFileStorageParams(
+      LoadFileUseCase.fillFileStorage,
+      FillFileStorageParams(
         rp.sendPort,
         repoPath,
         blobCTimeBuilder,
@@ -127,54 +123,4 @@ class FileStorage with ChangeNotifier {
 class FileStorageCacheIncomplete implements Exception {
   final String path;
   FileStorageCacheIncomplete(this.path);
-}
-
-typedef _FillFileStorageParams
-    = Tuple4<SendPort, String, BlobCTimeBuilder, FileMTimeBuilder>;
-
-typedef _FillFileStorageOutput
-    = Tuple3<BlobCTimeBuilder, FileMTimeBuilder, GitHash>;
-
-_FillFileStorageOutput? _fillFileStorage(_FillFileStorageParams params) {
-  var sendPort = params.item1;
-  var repoPath = params.item2;
-  var blobCTimeBuilder = params.item3;
-  var fileMTimeBuilder = params.item4;
-
-  var dateTime = DateTime.now();
-  var visitor = MultiTreeEntryVisitor(
-    [blobCTimeBuilder, fileMTimeBuilder],
-    afterCommitCallback: (commit) {
-      var commitDt = commit.author.date;
-      if (commitDt.isBefore(dateTime)) {
-        dateTime = commitDt;
-        sendPort.send(dateTime);
-      }
-    },
-  );
-
-  var gitRepo = GitRepository.load(repoPath).getOrThrow();
-  var headR = gitRepo.headHash();
-  if (headR.isFailure) {
-    if (headR.error is GitRefNotFound) {
-      // No commits
-      // fileStorageCacheReady = true;
-      // notifyListeners();
-      // FIXME: Send a signal saying its done
-      return _FillFileStorageOutput(
-          blobCTimeBuilder, fileMTimeBuilder, GitHash.zero());
-    }
-    Log.e("Failed to fetch HEAD", result: headR);
-    return null;
-  }
-  var head = headR.getOrThrow();
-  Log.d("Got HEAD: $head");
-
-  var result = gitRepo.visitTree(fromCommitHash: head, visitor: visitor);
-  if (result.isFailure) {
-    Log.e("Failed to build FileStorage cache", result: result);
-    logException(result.exception!, result.stackTrace!);
-  }
-
-  return _FillFileStorageOutput(blobCTimeBuilder, fileMTimeBuilder, head);
 }
--- lib/core/file/load_file_usecase.dart
+++ lib/core/file/load_file_usecase.dart
@@ -1,11 +1,23 @@
+import 'dart:isolate';
+
 import 'package:dart_git/blob_ctime_builder.dart';
+import 'package:dart_git/dart_git.dart';
+import 'package:dart_git/exceptions.dart';
 import 'package:dart_git/file_mtime_builder.dart';
 import 'package:gitjournal/core/file/file_storage.dart';
+import 'package:gitjournal/error_reporting.dart';
 import 'package:gitjournal/logger/logger.dart';
 import 'package:path/path.dart' as path;
+import 'package:tuple/tuple.dart';
 import 'package:universal_io/io.dart' as io;
 
 import 'file.dart';
+
+typedef FillFileStorageParams
+    = Tuple4<SendPort, String, BlobCTimeBuilder, FileMTimeBuilder>;
+
+typedef FillFileStorageOutput
+    = Tuple3<BlobCTimeBuilder, FileMTimeBuilder, GitHash>;
 
 class LoadFileUseCase {
   static File loadFile(
@@ -55,5 +67,49 @@ class LoadFileUseCase {
       created: created,
       modified: modified,
     );
+  }
+
+  static FillFileStorageOutput? fillFileStorage(FillFileStorageParams params) {
+    var sendPort = params.item1;
+    var repoPath = params.item2;
+    var blobCTimeBuilder = params.item3;
+    var fileMTimeBuilder = params.item4;
+
+    var dateTime = DateTime.now();
+    var visitor = MultiTreeEntryVisitor(
+      [blobCTimeBuilder, fileMTimeBuilder],
+      afterCommitCallback: (commit) {
+        var commitDt = commit.author.date;
+        if (commitDt.isBefore(dateTime)) {
+          dateTime = commitDt;
+          sendPort.send(dateTime);
+        }
+      },
+    );
+
+    var gitRepo = GitRepository.load(repoPath).getOrThrow();
+    var headR = gitRepo.headHash();
+    if (headR.isFailure) {
+      if (headR.error is GitRefNotFound) {
+        // No commits
+        // fileStorageCacheReady = true;
+        // notifyListeners();
+        // FIXME: Send a signal saying its done
+        return FillFileStorageOutput(
+            blobCTimeBuilder, fileMTimeBuilder, GitHash.zero());
+      }
+      Log.e("Failed to fetch HEAD", result: headR);
+      return null;
+    }
+    var head = headR.getOrThrow();
+    Log.d("Got HEAD: $head");
+
+    var result = gitRepo.visitTree(fromCommitHash: head, visitor: visitor);
+    if (result.isFailure) {
+      Log.e("Failed to build FileStorage cache", result: result);
+      logException(result.exception!, result.stackTrace!);
+    }
+
+    return FillFileStorageOutput(blobCTimeBuilder, fileMTimeBuilder, head);
   }
 }
